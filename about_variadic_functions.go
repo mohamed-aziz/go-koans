872f782f5b4cb4ab01598130f63755251b4c8dd8
mohamed-aziz <medazizknani@gmail.com>
2020-01-23 09:09:54 +0100

    Done koans

--- about_variadic_functions.go
+++ about_variadic_functions.go
@@ -1,6 +1,8 @@
 package go_koans
 
-import "strings"
+import (
+	"strings"
+)
 
 func concatNames(sep string, names ...string) string {
 	return strings.Join(names, sep) // variadic parameters are really just slices
@@ -9,12 +11,12 @@ func concatNames(sep string, names ...string) string {
 func aboutVariadicFunctions() {
 	{
 		str := concatNames(" ", "bob", "billy", "fred")
-		assert(str == __string__) // several values can be passed to variadic parameters
+		assert(str == "bob billy fred") // several values can be passed to variadic parameters
 	}
 
 	{
 		names := []string{"bob", "billy", "fred"}
 		str := concatNames("-", names...)
-		assert(str == __string__) // or a slice can be dotted in place of all of them
+		assert(str == "bob-billy-fred") // or a slice can be dotted in place of all of them
 	}
 }
